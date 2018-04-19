910ca23aa55b5e85603d6aa19b5fa6b779463dd9
Muzammil Hasan Momin <muzammilmomin@live.com>
2018-04-19 17:06:34 +0530

    Update vagrantInstaller.bat

--- vagrantInstaller.bat
+++ vagrantInstaller.bat
@@ -3,4 +3,13 @@ choco feature enable -n=allowGlobalConfirmation
 choco install vagrant 
 choco install virtualbox 
 choco install powershell
+choco install curl
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All"
+set /p drive="Enter Drive Letter To Install files in : "
+
+mkdir "%drive%:\vagrant\"
+cd /d "%drive%:\vagrant\"
+set /p password="Enter ftp password : "
+
+curl ftp://ftp.mpenv.com/setup/VagrantFileMongo --user sre:%password% -o %drive%:\vagrant\Vagrantfile
+vagrant up
